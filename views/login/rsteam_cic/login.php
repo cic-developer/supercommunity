4d5e8c0cb74fe79151fe8af56d353ea083307c0b
rstema-dev <rsteam-dev>
2021-03-18 01:29:59 +0000

    login delete

--- views/login/rsteam_cic/login.php
+++ views/login/rsteam_cic/login.php
@@ -6,7 +6,7 @@
 
 <?php
 if(
-	$this->input->ip_address() == '49.163.50.132'
+	$this->input->ip_address() == '49.163.50.132      /이거 지우면 로그인 보여요/'
 // || $this->input->ip_address() == '59.26.134.158'
 ){
 	echo validation_errors('<div class="alert alert-warning" role="alert">', '</div>');
