361f0f769759ea7bf5bf13e35adf8a54d55e0c3e
rimose <yop-ko@nate.com>
2021-04-21 05:53:21 +0000

    tttttest

--- views/popup/popup/paymentPolicy.php
+++ views/popup/popup/paymentPolicy.php
@@ -1,3 +1,34 @@
+<head>
+
+<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/ui-lightness/jquery-ui.css" />
+<?php echo $this->managelayout->display_css(); ?>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script type="text/javascript">
+// 자바스크립트에서 사용하는 전역변수 선언
+var cb_url = "<?php echo trim(site_url(), '/'); ?>";
+var cb_cookie_domain = "<?php echo config_item('cookie_domain'); ?>";
+var cb_charset = "<?php echo config_item('charset'); ?>";
+var cb_time_ymd = "<?php echo cdate('Y-m-d'); ?>";
+var cb_time_ymdhis = "<?php echo cdate('Y-m-d H:i:s'); ?>";
+var layout_skin_path = "<?php echo element('layout_skin_path', $layout); ?>";
+var view_skin_path = "<?php echo element('view_skin_path', $layout); ?>";
+var is_member = "<?php echo $this->member->is_member() ? '1' : ''; ?>";
+var is_admin = "<?php echo $this->member->is_admin(); ?>";
+var cb_admin_url = <?php echo $this->member->is_admin() === 'super' ? 'cb_url + "/' . config_item('uri_segment_admin') . '"' : '""'; ?>;
+var cb_board = "<?php echo isset($view) ? element('board_key', $view) : ''; ?>";
+var cb_board_url = <?php echo ( isset($view) && element('board_key', $view)) ? 'cb_url + "/' . config_item('uri_segment_board') . '/' . element('board_key', $view) . '"' : '""'; ?>;
+var cb_device_type = "<?php echo $this->cbconfig->get_device_type() === 'mobile' ? 'mobile' : 'desktop' ?>";
+var cb_csrf_hash = "<?php echo $this->security->get_csrf_hash(); ?>";
+var cookie_prefix = "<?php echo config_item('cookie_prefix'); ?>";
+var rs_lang = "<?php echo $this->session->userdata('lang'); ?>";
+</script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/common.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.validate.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.validate.extension.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/sideview.js?v=1'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/js.cookie.js'); ?>"></script>
+
 <!-- 팝업레이어 시작 -->
 <style type="text/css">
 /* html {
@@ -10,13 +41,14 @@
 .popup_layer_footer button {margin-right:10px;padding:5px 10px;border:0;background:#4F4F4F;color:#FFFFFF}
 /* .popup_contents {width: 500px; height: 300px;} */
 </style>
+</head>
+<body>
 
 	<div id="popup_layer" class="popup_layer">
 	<div class="popup_contents">
 		<?php echo $mis_payment_policy ?>
 	</div>
 	</div>
-
 
 <script type="text/javascript">
 //<![CDATA[
@@ -31,3 +63,4 @@ $(function() {
 //]]>
 </script>
 <!-- 팝업레이어 끝 -->
+</body>
--- views/popup/popup/popup.php
+++ views/popup/popup/popup.php
@@ -1,9 +1,42 @@
+<head>
+
+<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/ui-lightness/jquery-ui.css" />
+<?php echo $this->managelayout->display_css(); ?>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script type="text/javascript">
+// 자바스크립트에서 사용하는 전역변수 선언
+var cb_url = "<?php echo trim(site_url(), '/'); ?>";
+var cb_cookie_domain = "<?php echo config_item('cookie_domain'); ?>";
+var cb_charset = "<?php echo config_item('charset'); ?>";
+var cb_time_ymd = "<?php echo cdate('Y-m-d'); ?>";
+var cb_time_ymdhis = "<?php echo cdate('Y-m-d H:i:s'); ?>";
+var layout_skin_path = "<?php echo element('layout_skin_path', $layout); ?>";
+var view_skin_path = "<?php echo element('view_skin_path', $layout); ?>";
+var is_member = "<?php echo $this->member->is_member() ? '1' : ''; ?>";
+var is_admin = "<?php echo $this->member->is_admin(); ?>";
+var cb_admin_url = <?php echo $this->member->is_admin() === 'super' ? 'cb_url + "/' . config_item('uri_segment_admin') . '"' : '""'; ?>;
+var cb_board = "<?php echo isset($view) ? element('board_key', $view) : ''; ?>";
+var cb_board_url = <?php echo ( isset($view) && element('board_key', $view)) ? 'cb_url + "/' . config_item('uri_segment_board') . '/' . element('board_key', $view) . '"' : '""'; ?>;
+var cb_device_type = "<?php echo $this->cbconfig->get_device_type() === 'mobile' ? 'mobile' : 'desktop' ?>";
+var cb_csrf_hash = "<?php echo $this->security->get_csrf_hash(); ?>";
+var cookie_prefix = "<?php echo config_item('cookie_prefix'); ?>";
+var rs_lang = "<?php echo $this->session->userdata('lang'); ?>";
+</script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/common.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.validate.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.validate.extension.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/sideview.js?v=1'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/js.cookie.js'); ?>"></script>
+
 <!-- 팝업레이어 시작 -->
 <style type="text/css">
 .popup_layer {position:absolute;border:1px solid #e9e9e9;background:#fff}
 .popup_layer_footer {padding:10px 0;background:#000;color:#fff;text-align:right}
 .popup_layer_footer button {margin-right:10px;padding:5px 10px;border:0;background:#4F4F4F;color:#FFFFFF}
 </style>
+</head>
+<body>
 <?php
 	$result = $popup;
 ?>
@@ -65,3 +98,4 @@ $(function() {
 //]]>
 </script>
 <!-- 팝업레이어 끝 -->
+</body>
