4933104e8d5208ee34400a1d2cf3c4e7c9cbd0c2
rimose <yop-ko@nate.com>
2021-06-22 06:08:38 +0000

    모바일 사전가입 팝업

--- views/main/rsteam_cic_mobile/main.php
+++ views/main/rsteam_cic_mobile/main.php
@@ -1,3 +1,4 @@
+<script src="../../../assets/js/jquery.bpopup-0.1.1.min.js"></script>
 <?php 
     $csrf = array(
         'name' => $this->security->get_csrf_token_name(),
@@ -39,6 +40,37 @@
 	</div>
 </div>
 <!--//main-->
+
+<div id="element_to_pop_up">
+    <a href="https://www.ciccommunity.com/sign_up" style="width:100%;"><img src="../../../assets/images/popup_ver1.png" style="width:100%;"></a>
+    <p style="text-align:center;"><b style="font-size:20px;">[cic 커뮤니티 오픈기념 사전가입 이벤트]</b><br><br>
+        안녕하세요 퍼 프렌즈 여러분<br>
+        신개념 코인 커뮤니티 cic 커뮤니티에서 사전가입 이벤트를 진행합니다<br>
+        지금 사전가입을 하시면 1000vp와 5cp, 그리고 추첨을 통해 특별한 선물을 드립니다<br>
+        해당 이벤트는 2021년 6월 30일 12:00 까지 진행됩니다<br><br>
+        <span style="font-weight:bold; color:#f40315;">*특별한 선물은 지갑주소에 PER Wallet 또는<br> Kaikas 주소를 입력한 분들에 한정합니다*</span>
+    </p>
+</div>
+<style>
+    #element_to_pop_up {
+        background-color: #fff;
+        border-radius: 15px;
+        color: #000;
+        display: none;
+        padding: 20px;
+        min-width: 150px;
+        min-height: 180px;
+        width:80%;
+        padding: 0;
+        padding-bottom: 10px;
+        margin-top: 50px;
+    }
+</style>
+<script>
+    $(document).ready(function() {
+        $('#element_to_pop_up').bPopup({});
+    })
+</script>
 
 
 
